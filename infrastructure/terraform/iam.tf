b754e2ea2209ede4a5eb9568f6d18ecb715e233b
George Omololu Akinbi <georgelolu@gmail.com>
2026-09-16 11:52:03 +0100

    docs: finalize DevOps documentation and monitoring

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -35,6 +35,11 @@ resource "aws_iam_role_policy_attachment" "ec2_ecr" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "ec2_cloudwatch_agent" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 resource "aws_iam_instance_profile" "ec2" {
   name = "${var.project_name}-ec2-profile"
   role = aws_iam_role.ec2.name
